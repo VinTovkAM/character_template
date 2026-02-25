d72a3cf77df0b9e989b7d74094d28e81813a1bd2
Dimka <vashetopakk@gmail.com>
2026-02-25 19:06:34 +0300

    Added the basic logic of the code

--- template.py
+++ template.py
@@ -1,16 +1,105 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+import random
+import os
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html'])
-)
-template = env.get_template('template.html')
 
-# пишем код для получения данных карточки
+CLASSES_BASE = {
+        'Охотник': {
+            'skills': ['Верный выстрел', 'Чародейский выстрел', 'Стенающая стрела', 'Стрелы ветра', 'Призыв питомца', 'Глаз зверя', 'Осветительная ракета', 'Приручение животного'],
+            'strength': random.randint(1, 3),
+            'agility': 15,
+            'intelligence': random.randint(1, 3),
+            'luck': random.randint(1, 3),
+            'temper': random.randint(1, 3),
+            'image': r'images\archer.png'
+        },
+        'Маг': {
+            'skills': ['Стрела ледяного огня', 'Снятие проклятия', 'Огненный взрыв', 'Обледенение', 'Ледяное копье', 'Конус холода', 'Прилив сил', 'Морозный доспех'],
+            'strength': random.randint(1, 3),
+            'agility': random.randint(1, 3),
+            'intelligence': 15,
+            'luck': random.randint(1, 3),
+            'temper': random.randint(1, 3),
+            'image': r'images\wizard.png'
+        },
+        'Ассасин': {
+            'skills': ['Отравление', 'Взлом замка', 'Подлый трюк', 'Исчезновение', 'Ложный выпад', 'Внезапный удар', 'Ошеломление', 'Спринт'],
+            'strength': random.randint(1, 3),
+            'agility': random.randint(1, 3),
+            'intelligence': random.randint(1, 3),
+            'luck': 15,
+            'temper': random.randint(1, 3),
+            'image': r'images\assasin.png'
+        },
+        'Бард': {
+            'skills': ['Аккорды ветра', 'Аккорды воды', 'Исцеление', 'Соната жизни', 'Пауза', 'Плач сирен', 'Песнь ветра', 'Реквием'],
+            'strength': random.randint(1, 3),
+            'agility': random.randint(1, 3),
+            'intelligence': random.randint(1, 3),
+            'luck': random.randint(1, 3),
+            'temper': 15,
+            'image': r'images\bard.webp'
+        },
+        'Воин': {
+            'skills': ['Блок щитом', 'Казнь', 'Рывок', 'Боевой крик', 'Вихрь', 'Парирование', 'Мощный удар', 'Глубокие раны'],
+            'strength': 15,
+            'agility': random.randint(1, 3),
+            'intelligence': random.randint(1, 3),
+            'luck': random.randint(1, 3),
+            'temper': random.randint(1, 3),
+            'image': r'images\warrior.png'
+        },
+    }
 
-rendered_page = template.render(
-    # пишем код, что добавляется к карточке
-)
+def main():
+    folder = 'characters'
+    if not os.path.exists(folder):
+        os.makedirs(folder)
 
-with open(f'index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html'])
+    )
+    template = env.get_template('template.html')
+
+    character_class_list = ['Охотник', 'Ассасин', 'Бард', 'Воин', 'Маг'] 
+    race_list = ['Человек', 'Орк', 'Эльф', 'Дварф']
+     
+    quality_page = int(input('Сколько вам нужно карточек(цифрой): '))
+
+    for page in range(quality_page):
+        name_hero = input('Введите имя героя: ')
+
+        for number, race in enumerate(race_list, 1):
+            print(f'{number}. {race}')
+
+        race_hero = int(input('Выберите расу: '))
+
+        for number, character_class in enumerate(character_class_list, 1):
+            print(f'{number}. {character_class}')
+
+        character_class_hero = int(input('Выберите класс: '))
+
+        skills = random.sample(CLASSES_BASE[character_class]['skills'], 3)
+
+        rendered_page = template.render(
+            name=name_hero,
+            race=race_list[race_hero-1], 
+            character_class=character_class_list[character_class_hero-1],
+            strength=CLASSES_BASE[character_class]['strength'],
+            agility=CLASSES_BASE[character_class]['agility'],
+            intelligence=CLASSES_BASE[character_class]['intelligence'],
+            luck=CLASSES_BASE[character_class]['luck'],
+            temper=CLASSES_BASE[character_class]['temper'],
+            image=CLASSES_BASE[character_class]['image'],
+            first_skill=skills[0],
+            second_skill=skills[1],
+            third_skill=skills[2]
+        )
+
+        with open(os.path.join(folder, f'index{page+1}.html'), 'w', encoding="utf8") as file:
+            file.write(rendered_page)
+
+
+if __name__ == '__main__':
+    main()
